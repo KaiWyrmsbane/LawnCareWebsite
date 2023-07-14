3afee8916ee31c6dc69ed8fb2ebf4df0be36c61e
James VanMeter <jamesvanmeter1219@gmail.com>
2023-07-13 20:32:53 -0400

    fixing the table presenting the array

--- scripts/APICode.js
+++ scripts/APICode.js
@@ -1,12 +1,12 @@
-fetch('https://api.weatherbit.io/v2.0/current', {
-    method : 'post',
-    headers: {
-        'Authorization': 'Basic ' + btoa('kaiWyrmsbane:4c84b72661e14aa196e635179c13c3fd')
-    }
+fetch("https://api.weatherbit.io/v2.0/current", {
+  method: "post",
+  headers: {
+    Authorization:
+      "Basic " + btoa("kaiWyrmsbane:4c84b72661e14aa196e635179c13c3fd"),
+  },
 })
-
-.then(response => {
-// Do something with the response here
-console.log(response);
-})
-.catch(error => console.log(error))
+  .then((response) => {
+    // Do something with the response here
+    console.log(response);
+  })
+  .catch((error) => console.log(error));
--- scripts/store.js
+++ scripts/store.js
@@ -1,55 +1,58 @@
-              //create an array of 10 lawnmowers
-              let mowers = [
-                {
-                    "Brand":"Cub Cadet",
-                    "type": "Riding Mower"
-                },
-                {
-                    "Brand":"Troy Bilt",
-                    "type": "Riding Mower"
-                },
-                {
-                    "Brand":"Troy Bilt",
-                    "type": "Riding Mower"
-                },
-                {
-                    "Brand":"Troy Bilt",
-                    "type": "Riding Mower"
-                },
-                {
-                    "Brand":"Troy Bilt",
-                    "type": "Riding Mower"
-                },
-                {
-                    "Brand":"Troy Bilt",
-                    "type": "Riding Mower"
-                },
-                {
-                    "Brand":"Scag",
-                    "type": "Zero-Turn Mower"
-                },
-                {
-                    "Brand":"Craftsman",
-                    "type": "Push Mower"
-                },
-                {
-                    "Brand":"Craftsman",
-                    "type": "Zero-Turn Mower"
-                },
-                {
-                    "Brand":"Craftsman",
-                    "type": "Riding Mower"
-                },
-            ];
+//create an array of 10 lawnmowers
+let mowers = [
+  {
+    Brand: "Cub Cadet",
+    type: "Riding Mower",
+  },
+  {
+    Brand: "Troy Bilt",
+    type: "Riding Mower",
+  },
+  {
+    Brand: "Troy Bilt",
+    type: "Riding Mower",
+  },
+  {
+    Brand: "Troy Bilt",
+    type: "Riding Mower",
+  },
+  {
+    Brand: "Troy Bilt",
+    type: "Riding Mower",
+  },
+  {
+    Brand: "Troy Bilt",
+    type: "Riding Mower",
+  },
+  {
+    Brand: "Scag",
+    type: "Zero-Turn Mower",
+  },
+  {
+    Brand: "Craftsman",
+    type: "Push Mower",
+  },
+  {
+    Brand: "Craftsman",
+    type: "Zero-Turn Mower",
+  },
+  {
+    Brand: "Craftsman",
+    type: "Riding Mower",
+  },
+];
 
-            function buildTable(data){
-                const table = document.getElementById("mowersInStock");
-    
-                for(let i = 0; i < data.length; i++){
-                    const row = <tr>
-                                    <td>${data[i].Brand}</td>
-                                    <td>${data[i].type}</td>
-                              </tr>;
-                    table.innerHTML += row;
-                }
-            }
+buildTable(mowers);
+
+function buildTable(data) {
+  const table = document.getElementById("mowers");
+
+  for (let i = 0; i < data.length; i++) {
+    let row = `<tr>
+        <td>${data[i].Brand}</td>
+        <td>${data[i].type}</td>
+      </tr>`;
+
+    table.innerHTML += row;
+  }
+}
