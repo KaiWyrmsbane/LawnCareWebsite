a87eae4572a5705805b0675a23eeb7da15eb10ba
James VanMeter <jamesvanmeter1219@gmail.com>
2023-08-03 19:18:47 -0400

    edited the api where it will have a 7 day forecast with high and low tempsbut was unable to test with the limited calls I can make, also making the overall website better looking!

--- scripts/APICode.js
+++ scripts/APICode.js
@@ -1,14 +1,27 @@
 //https://www.youtube.com/watch?v=MIYQR-Ybrn4&t=1118s
 const AppKey = "4c84b72661e14aa196e635179c13c3fd";
-const AppUrl = "https://api.weatherbit.io/v2.0/current?postal_code=40165";
+const AppUrl =
+  "https://api.weatherbit.io/v2.0/forecast/daily?postal_code=40165&country=us&days=7&units=I&lang=el";
 
 async function CheckWeather() {
-  const responce = await fetch(AppUrl + `&key=${AppKey}`);
-  var data = await responce.json();
+  const response = await fetch(AppUrl + `&key=${AppKey}`);
+  var data = await response.json();
 
   console.log(data);
-  document.querySelector(".cityname").innerHTML = data.data[0].city_name;
-  document.querySelector(".temp").innerHTML = data.data[0].app_temp;
+  document.querySelector(".dayOne").innerHTML =
+    data.data[0].low_temp + "-" + data.data[0].high_temp;
+  document.querySelector(".dayTwo").innerHTML =
+    data.data[1].low_temp + "-" + data.data[1].high_temp;
+  document.querySelector(".dayThree").innerHTML =
+    data.data[2].low_temp + "-" + data.data[2].high_temp;
+  document.querySelector(".dayFour").innerHTML =
+    data.data[3].low_temp + "-" + data.data[3].high_temp;
+  document.querySelector(".dayFive").innerHTML =
+    data.data[4].low_temp + "-" + data.data[4].high_temp;
+  document.querySelector(".daySix").innerHTML =
+    data.data[5].low_temp + "-" + data.data[5].high_temp;
+  document.querySelector(".daySeven").innerHTML =
+    data.data[6].low_temp + "-" + data.data[6].high_temp;
 }
 
 CheckWeather();
