e5cb23df31178a5c8f922f7cb20e74985f839dc9
James VanMeter <jamesvanmeter1219@gmail.com>
2023-08-02 21:37:21 -0400

    got my filter section working

--- scripts/store.js
+++ scripts/store.js
@@ -58,8 +58,37 @@ function buildTable(data) {
 }
 
 //use this funtion to filtrate the Array of lawnmowers
-function filterTable(brand, type) {
-  if (brand == "Scag") {
-    console.log(mowers[6].Brand);
+FilterObjects("all");
+
+function FilterObjects(c) {
+  let x, i;
+  x = document.getElementsByClassName("box");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
   }
+}
+
+function AddClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function RemoveClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
 }
